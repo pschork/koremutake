d863bb41ce1f96fe608d2408f91ecbba2269bc3a
Patrick Schork <pschork@twitter.com>
2012-07-20 12:19:21 -0700

    refactor packaging

--- koremutake.py
+++ koremutake.py
@@ -12,10 +12,8 @@ See http://shorl.com/koremutake.php for details
 >>> koremutake.decode('koremutake')
 10610353957
 """
-# Encoding algorithm adapted from http://code.activestate.com/recipes/111286
+import unittest
 
-__name__ = 'koremutake'
-__version__ = '1.0.4'
 __author__ = 'Patrick Schork <patrick@schork.com>'
 
 __all__ = ['encode','decode']
@@ -35,7 +33,8 @@ phonemes = ['ba','be','bi','bo','bu','by','da','de','di','do','du','dy','fa',
 def encode(value):
     "Encode unsigned integer value to Koremutake string"
     key = ""
-    assert value >= 0, 'Value to encode must be unsigned'
+    if value < 0: 
+        raise ValueError('Encode value must be a positive integer')
     if value == 0:
         return phonemes[0]
     while value > 0:
@@ -49,11 +48,48 @@ def decode(value):
     orig = value
     x=long(0)
     while value:
-        assert value[:2] in phonemes or value[:3] in phonemes,\
-            "Failed to decode '%s' @ %s digit" % (orig, len(orig) - len(value))
         if value[:2] in phonemes:
             bit,value = value[:2],value[2:]
         else:
             bit,value = value[:3],value[3:]   
         x = x*len(phonemes) + phonemes.index(bit)
     return x
+
+class test(unittest.TestCase):
+    def test_encode(self):
+        self.assertEqual(encode(0), "ba")
+        self.assertEqual(encode(127), "tre")
+        self.assertEqual(encode(128), "beba")
+        self.assertEqual(encode(256), "biba")
+        self.assertEqual(encode(128**2), "bebaba")
+        self.assertEqual(encode(128**2 - 1), "tretre")
+        self.assertEqual(encode(128**3), "bebababa")
+        self.assertEqual(encode(128**3 - 1), "tretretre")
+        self.assertEqual(encode(10610353957), "koremutake")
+        self.assertEqual(encode(4398046511103), "tretretretretretre")
+
+        # Negative numbers can't be converted
+        self.assertRaises(ValueError, encode, -1)
+
+    def test_decode(self):
+        self.assertEqual(decode("ba"), 0)
+        self.assertEqual(decode("tre"),127)
+        self.assertEqual(decode("beba"), 128)
+        self.assertEqual(decode("biba"), 256)
+        self.assertEqual(decode("bebaba"), 128**2)
+        self.assertEqual(decode("tretre"), 128**2 - 1)
+        self.assertEqual(decode("bebababa"), 128**3)
+        self.assertEqual(decode("tretretre"), 128**3 - 1)
+        self.assertEqual(decode("koremutake"), 10610353957)
+        self.assertEqual(decode("tretretretretretre"), 4398046511103)
+        self.assertEqual(decode("baba"), 0)
+        self.assertEqual(decode("bababababa"), 0)
+        self.assertEqual(decode("babatre"), 127)
+
+        # Invalid koremutake strings
+        self.assertRaises(ValueError, decode, "foo")
+        self.assertRaises(ValueError, decode, "kooremutake")
+        self.assertRaises(ValueError, decode, "bab")
+
+if __name__ == '__main__':
+    unittest.main()
--- setup.py
+++ setup.py
@@ -3,24 +3,34 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
-import os
-import koremutake
+version = '1.0.5'
+short_desc = "Convert to and from Koremutake Memorable Random Strings",
+long_desc = """Koremutake is a 128bit MeRS encoding algorithm that can convert any large, 
+unsigned number into a memorable sequence of phonetically unique syllables.
 
-readme = file(os.path.join(os.path.dirname(__file__), 'README')).readlines()
-short_desc = readme[0].strip()
-long_desc = "".join(readme[2:])
+For details see http://shorl.com/koremutake.php
 
-setup(name='koremutake',
-      version = koremutake.__version__,
-      description = short_desc,
-      packages = ['koremutake'],
-      long_description = long_desc,
-      keywords = 'koremutake kormutake koremutate kormutate shorl mers tinyurl',
-      author = 'Patrick Schork',
-      author_email = 'patrick@schork.com',
-      maintainer = 'Patrick Schork',
-      maintainer_email = 'patrick@schork.com',
-      url = 'http://github.com/pschork/koremutake'
-      license = 'BSD',
-      test_suite = 'test',
+To install, run 'sudo easy_install koremutake'
+
+>>> import koremutake
+>>> koremutake.encode(10610353957)
+'koremutake'
+>>> koremutake.decode('koremutake')
+10610353957
+"""
+
+setup(
+    name='koremutake',
+    py_modules = ['koremutake'],
+    version = version,
+    description = short_desc,
+    long_description = long_desc,
+    keywords = 'koremutake kormutake koremutate kormutate shorl mers tinyurl',
+    author = 'Patrick Schork',
+    author_email = 'patrick@schork.com',
+    maintainer = 'Patrick Schork',
+    maintainer_email = 'patrick@schork.com',
+    url = 'http://github.com/pschork/koremutake',
+    license = 'BSD',
+    test_suite = 'koremutake',
     )
